f43b1fdc7f707ddb932222169780678d080f1caf
feyi-codes2 <feyifireamen@gmail.com>
2022-09-14 17:00:28 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-program that prints -putchar
+ * returns:always 0
+ */
+int main(void)
+{
+	putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return(0);
+}
+
+
